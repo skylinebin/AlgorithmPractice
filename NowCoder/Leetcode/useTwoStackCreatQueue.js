d8a38abb4a310f543efb4c76f10d7c57de344e0f
SkylineBin <xyliubin@outlook.com>
2018-09-14 00:10:40 +0800

    add two stack to create one Queue

---
+++ NowCoder/Leetcode/useTwoStackCreatQueue.js
@@ -0,0 +1,26 @@
+/****
+ * 
+ * 用两个栈来实现一个队列， 完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * 
+ */
+
+ // first version
+
+ var stack1 = [],
+     stack2 = [];
+
+ function push(node) {
+     // write code here
+     stack1.push(node);
+ }
+
+ function pop() {
+     if (stack2.length === 0) {
+         while (stack1.length !== 0) {
+             stack2.push(stack1.pop());
+         }
+         return stack2.pop();
+     } else {
+         return stack2.pop();
+     }
+ }
