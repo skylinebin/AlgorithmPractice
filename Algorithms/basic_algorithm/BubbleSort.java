38abcde034ef50ccd76b3da47ec7e8055d47499b
SkylineBin <xyliubin@outlook.com>
2018-10-03 14:00:48 +0800

    加入大样本环境测试，并对比自己优化的算法与绝对正确的算法的不同
    以及一些辅助的函数

--- Algorithms/basic_algorithm/BubbleSort.java
+++ Algorithms/basic_algorithm/BubbleSort.java
@@ -60,7 +60,78 @@ public class BubbleSort {
 		
 	}
 	
+//	for test
+//	生成拷贝数组的方法
+	public static int[] copyArray(int[] arr) {
+		if (arr == null) {
+			return null;
+		}
+		int[] res = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			res[i] = arr[i];
+		}
+		return res;
+	}
+
+//	for test
+//	判断两个数组是否相同
+	public static boolean isEqual(int[] arrone, int[] arrtwo) {
+		if((arrone == null && arrtwo != null) || (arrone != null && arrtwo == null)) {
+			return false;
+		}
+		if(arrone == null && arrtwo == null) {
+			return true;
+		}
+		if(arrone.length != arrtwo.length) {
+			return false;
+		}
+		for (int i = 0; i < arrone.length; i++) {
+			if (arrone[i] != arrtwo[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 	
+//	for test
+//	打印数组的方法
+	public static void printArray(int[] arr) {
+		if(arr == null || arr.length ==0) {
+			System.out.println("");
+		}else {
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i]);
+				System.out.println("");
+			}
+		}
+	}
+	
+
+//	大样本测试 方法
+	public static void main(String[] args) {
+		int testTime = 500000;
+		int size = 10;
+		int value = 100;
+		boolean succeed = true;
+		for (int i = 0; i < testTime; i++) {
+			int[] arrone = generateRandomArray(size, value);
+			int[] arrtwo = copyArray(arrone);
+			int[] arrthree = copyArray(arrone);
+			bubbleSort(arrone);
+			rightMethod(arrtwo);
+			if(!isEqual(arrone, arrtwo)) {
+				succeed = false;
+				printArray(arrthree);
+				break;
+			}
+		}
+		
+		System.out.println(succeed ? "well done!" : "wrong algorithms!");
+		
+		int[] arr = generateRandomArray(size, value);
+		printArray(arr);
+		
+	}
 	
 	
 	
