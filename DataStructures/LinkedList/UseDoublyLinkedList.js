69a040212bb10f717d3e63b62d979f4f2e5e8fab
SkylineBin <xyliubin@outlook.com>
2018-10-10 12:44:40 +0800

    add DoublyLinkedList Structure

---
+++ DataStructures/LinkedList/UseDoublyLinkedList.js
@@ -0,0 +1,30 @@
+/****
+ * 
+ * @author SkylineBin
+ * @time 2018-10-10
+ * @function create doublyLinkedList by JavaScript
+ * 
+ */
+
+ function DoublyLinkedList( ) {
+     let Node = function(element) {
+         this.element = element;
+         this.next = null;
+         this.prev = null;
+     }
+
+     let length = 0;
+     let head = null;
+     let tail = null;
+
+    //  insert a element at this position
+     this.insert = function(position,element) {
+         
+     };
+
+    //  remove element at this position
+    this.removeAt = function(position) {
+        
+    };
+
+ }
