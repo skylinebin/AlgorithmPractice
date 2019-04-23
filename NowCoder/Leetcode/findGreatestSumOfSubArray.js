6438ecf35fb448a907c25911fa31f376b1a6c237
SkylineBin <xyliubin@outlook.com>
2019-04-23 21:27:55 +0800

    动态规划的思想解决最大连续子序列的和问题

---
+++ NowCoder/Leetcode/findGreatestSumOfSubArray.js
@@ -0,0 +1,38 @@
+/*****
+ * 
+ * 最大连续子序列的和
+ * 
+ * 在古老的一维模式识别中,常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。
+ * 但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？
+ * 例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
+ * 给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+ * 
+ * 
+ * 
+ */
+
+
+// 解题思路：动态规划思想
+// max 表示以array[i]为末尾元素的子数组的和的最大值，子数组的元素的相对位置不变
+// res 表示所有子数组和的最大值
+
+function FindGreatestSumOfSubArray(array) {
+    // write code here
+    if (array.length<=0 ) {
+        return;
+    }
+    if (array.length === 0) {
+        return array[0];
+    }
+    var res = array[0];
+    var max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        max = Math.max(max + array[i], array[i]);
+        res = Math.max(max, res);
+    }
+    return res;
+}
+
+var arr = [-5, -3, 6, -3, -2, 7, -15, 1, 2, 2];
+
+console.log(FindGreatestSumOfSubArray(arr));
