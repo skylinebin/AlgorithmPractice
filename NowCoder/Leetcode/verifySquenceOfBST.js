6c17a5fff007f6138dc3da012ce09522f9596465
SkylineBin <xyliubin@outlook.com>
2019-04-26 17:10:44 +0800

    验证二叉搜索树的后续遍历序列是否合法
    递归方法

--- NowCoder/Leetcode/verifySquenceOfBST.js
+++ NowCoder/Leetcode/verifySquenceOfBST.js
@@ -8,5 +8,31 @@
  */
 
 function VerifySquenceOfBST(sequence) {
-    // write code here
+    if (sequence.length === 0) {
+        return false;
+    }
+    if (sequence.length === 1) {
+        return true;
+    }
+    var len = sequence.length;
+    var rootNode = sequence[len-1];
+    var i;
+    for (i = 0; i < len-1; i++) {
+        if (sequence[i] > rootNode) {
+            break;
+        }
+    }
+    // i 为右子树的划分点
+    for (var j = i; j < len-1; j++) {
+        if (sequence[j] < rootNode) {
+            return false;
+        }
+    }
+
+    var checkLeft = (i > 0) ? VerifySquenceOfBST(sequence.slice(0,i)):true;
+    var checkRight = (i < len-1)? VerifySquenceOfBST(sequence.slice(i, len-1)):true;
+    return (checkLeft && checkRight);
 }
+
+var seq = [1,5,7,6,3,9,8]; // 二叉搜索树的后续遍历序列
+console.log(VerifySquenceOfBST(seq));
