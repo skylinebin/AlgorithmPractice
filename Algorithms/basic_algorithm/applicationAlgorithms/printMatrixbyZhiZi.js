533bb0c3ddfb1592c556068b4aa24e567e2ccef0
SkylineBin <xyliubin@outlook.com>
2019-04-29 19:49:58 +0800

    添加简洁版本的之字形打印解法

--- Algorithms/basic_algorithm/applicationAlgorithms/printMatrixbyZhiZi.js
+++ Algorithms/basic_algorithm/applicationAlgorithms/printMatrixbyZhiZi.js
@@ -41,7 +41,7 @@ function printMatrixbyZhiZi(matrix){
     }
 
     this.printDiagonal(matrix, a[0], a[1], b[0], b[1], rttolb);
-    while(true){
+    while (a[0] !== rr) {
         rttolb = !rttolb;
         if (a[1] < rc) {
             a[1] = a[1]+1;
@@ -54,12 +54,10 @@ function printMatrixbyZhiZi(matrix){
         } else if (b[0] === rr && b[1] < rc){
             b[1] = b[1] + 1;
         }
-
-
         this.printDiagonal(matrix, a[0], a[1], b[0], b[1], rttolb);
-        if (a[0] === rr && a[1] === rc) {
-            break;
-        }
+        // if (a[0] === rr && a[1] === rc) {
+        //     break;
+        // }
     }
     return output;
 
@@ -72,3 +70,50 @@ matrix[2] = [9, 10, 11, 12];
 matrix[3] = [13, 14, 15, 16];
 
 console.log(printMatrixbyZhiZi(matrix));
+
+
+// 简洁版本的写法
+function printMatrixbyZhiZi2(matrix) {
+    var ar = 0;
+    var ac = 0;
+    var br = 0;
+    var bc = 0;
+    var endr = matrix.length - 1;
+    var endc = matrix[0].length - 1;
+
+
+    var output = [];
+    var rttolb = true; // 用来标记是从右上向左下打印(true)，还是从左下向右上打印(false)
+
+
+    this.printDiagonal = function (matrix, trr, trc, tlr, tlc, rtol) {
+        if (rtol) {
+            // 从 右上 到 左下 打印
+            while (tlc <= trc && trr <= tlr) {
+                output.push(matrix[trr][trc]);
+                trr++;
+                trc--;
+            }
+        } else {
+            // 从 左下 到 右上 打印
+            while (tlc <= trc && trr <= tlr) {
+                output.push(matrix[tlr][tlc]);
+                tlr--;
+                tlc++;
+            }
+        }
+    }
+
+    while(ar !== endr + 1){
+        this.printDiagonal(matrix, ar, ac, br, bc, rttolb);
+        ar = ac === endc ? ar + 1 : ar;
+        ac = ac === endc ? ac : ac + 1;
+        bc = br === endr ? bc + 1 : bc;
+        br = br === endc ? br : br + 1;
+        rttolb = !rttolb;
+    }
+
+    return output;
+}
+
+console.log(printMatrixbyZhiZi2(matrix));
