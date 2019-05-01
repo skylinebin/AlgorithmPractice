7220f7cf1480f490b404e0d5773e0e302066113d
SkylineBin <xyliubin@outlook.com>
2019-05-01 14:50:19 +0800

    将一个字符串转换成一个整数的解决方法

--- NowCoder/Leetcode/strToInt.js
+++ NowCoder/Leetcode/strToInt.js
@@ -9,6 +9,8 @@
  * 
  */
 
+// 后续可以加上是否溢出的判断
+
 function StrToInt(str) {
     
     if (str === null || str.length === 0 || str === '+' || str === '-') {
@@ -16,6 +18,28 @@ function StrToInt(str) {
     }
     let start = 0;
     let pn = 0; // 用于判断该数是正是负
-
-
+    if (str[0] === '+'){
+        pn = 0;
+    }else if (str[0] === '-') {
+        pn = 1;
+    } else if (str[0] < '0' || str[0] > '9') {
+        return 0;
+    }else {
+        start = str[0] - '0';
+    }
+    let i = 1;
+    while (str[i]) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            start = start * 10 + (str[i] - '0');
+        } else {
+            return 0;
+        }
+        i++;
+    }
+    start = pn === 1? -1*start:start;
+    return start;
 }
+
+let str = '+2147483647';
+let str1 = '1a33';
+console.log(StrToInt(str1));
