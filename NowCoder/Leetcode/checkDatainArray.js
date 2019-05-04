d30c122aef942923eb9f4e9d7a3cb89a4163b656
SkylineBin <xyliubin@outlook.com>
2019-05-04 19:57:42 +0800

    利用矩阵有序的这一条件解决二维数组中查找值的问题

--- NowCoder/Leetcode/checkDatainArray.js
+++ NowCoder/Leetcode/checkDatainArray.js
@@ -18,10 +18,28 @@ function Find(target, array) {
     return false;
 }
 
- /* 
+/* 
  * 涉及理论： 搜索及排序
  * 普通解法： 遍历
- * 改进方向：
+ * 改进方向：利用矩阵有序的这一条件
+ * 改进方案：从左下角开始找起，比左下角小的向上移，比左下角大的向右移动
  *
  *
   */
+
+function Find2(target, array) {
+    let lenr = array.length;
+    let lenc = array[0].length;
+    let i = lenr - 1,
+        j = 0;
+    while (i >=0 && j<= lenc-1) {
+        if (array[i][j] < target) {
+            j++;
+        } else if (array[i][j] > target) {
+            i--;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
