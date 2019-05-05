fcd5240864aa5ebb88fd3745bb1abe0d9ef220d8
SkylineBin <xyliubin@outlook.com>
2019-05-05 14:30:26 +0800

    判断一个字符串是否表示数值 考虑字符多种可能性的解法

--- NowCoder/Leetcode/isNumeric.js
+++ NowCoder/Leetcode/isNumeric.js
@@ -14,8 +14,44 @@
 
 // 考虑可能性的解法
 function isNumeric(s) {
-    
+    if (s.length === 0 || s === null){
+        return false;
+    }
+    let nDolt =false; // 用于记录小数点 是否出现过
+    let nSymbol = false; // 用于 标记 +- 符号是否出现过
+    let nTen = false; // 用于标记 e 是否出现过
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'e' || s[i] === 'E') {
+            // e 后面没有数字 或者 已经出现过一次 E
+            if (i === s.length-1 || nTen) {
+                return false;
+            }
+            nTen = true;
+        }else if (s[i] === '+' || s[i] === '-') {
+            // 已经出现一次符号，这一次出现的符号前一个字符不是 'e' 或者 'E'
+            if (nSymbol && s[i-1] !== 'e' && s[i-1] !== 'E') {
+                return false;
+            }
+            if (!nSymbol && i > 0 && s[i - 1] !== 'e' && s[i - 1] !== 'E') {
+                return false;
+            }
+            nSymbol = true;
+        }else if (s[i] === '.'){
+            // 出现了两个小数点或者在E以后出现小数点都不是合法的数字
+            if (nDolt || nTen) {
+                return false;
+            }
+            nDolt = true;
+        } else if(s[i] < '0' || s[i] > '9'){
+            return false;
+        }
+    }
+    return true;
 }
+
+let s = '-1E-16';
+console.log(isNumeric(s));
 
 
 // 使用正则表达式简写
@@ -29,5 +65,5 @@ function isNumeric2(s) {
 // 奇怪的测试用例
 // "-.123" 是对的？
 
-let s = '5e2';
+
 console.log(isNumeric2(s));
