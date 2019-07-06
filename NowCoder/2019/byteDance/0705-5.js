6826053ebc007deaf2f6ac7b5d8b151ab0b768cf
SkylineBin <xyliubin@outlook.com>
2019-07-06 16:33:44 +0800

    网络节点之间的最大时延问题

---
+++ NowCoder/2019/byteDance/0705-5.js
@@ -0,0 +1,75 @@
+/*
+ * @Author: SkylineBin 
+ * @Date: 2019-07-06 15:47:41 
+ * @Last Modified by: SkylineBin
+ * @Last Modified time: 2019-07-06 16:33:24
+ */
+
+
+/****
+ * 
+ * 网络节点之间的最大时延问题
+ * 
+ * 
+ * 
+ */
+
+
+function TreeNode(x) {
+    this.val = x;
+    this.left = null;
+    this.right = null;
+}
+
+function findMaxTime(numN,arrA, nodeB){
+    let tempArr = [];
+    for (let i = 0; i < numN; i++) {
+        tempArr[i]=[];
+        for (let j = 0; j < numN; j++) {
+            tempArr[i].push(0);
+        }
+    }
+    let tempRoad;
+    // 构建 n*n 的矩阵
+    for (let i = 0; i < arrA.length; i++) {
+        tempRoad = arrA[i];
+        tempArr[parseInt(tempRoad[0]-1)][parseInt(tempRoad[1]-1)] = parseInt(tempRoad[2]);
+        tempArr[parseInt(tempRoad[1]-1)][parseInt(tempRoad[0]-1)] = parseInt(tempRoad[2]);
+    }
+    function checkMandN(m,n,tempArr){
+        for (let i = 0; i < tempArr.length; i++) {
+            if(tempArr[i][m]!==0 && tempArr[i][n]!==0){
+                if(!tempArr[m][n]){
+                    tempArr[m][n] = tempArr[i][m] + tempArr[i][n];
+                }else {
+                    tempArr[m][n] = Math.min(tempArr[i][m] + tempArr[i][n],tempArr[m][n]);
+                }
+                tempArr[n][m] = tempArr[m][n];
+            }
+        }
+    }
+    for (let m = 0; m < tempArr.length; m++) {
+        for (let n = 0; n < tempArr[0].length; n++) {
+            if(m !==n && tempArr[m][n] ===  0 && tempArr[n][m]===0){
+                checkMandN(m,n,tempArr);
+            }
+        }
+    }
+    // console.log(tempArr);
+    let outside = [];
+    let node;
+    for (let j = 0; j < nodeB.length; j++) {
+        node = tempArr[parseInt(nodeB[j]-1)];
+        node = node.sort(function(a,b){return a-b});
+        outside.push(node[node.length-1]);
+    }
+    return outside;
+}
+
+let numN = 4;
+let arrA = [[1,2,10],[1,3,5],[4,1,3]];
+let nodeB = [2,4];
+console.log(findMaxTime(numN,arrA, nodeB));
+
+// 部分案例可通过
+// 且效率应该不高
